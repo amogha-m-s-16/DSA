6aaaf3563c9b1712c13e527e8e887cec481e6f02
AMOGHA M S <amogha@kvgce.ac.in>
2026-08-09 19:18:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (20.31%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,36 +1,28 @@
 class Solution {
+private: 
+    int helper(const string& s, int i, long long num, int sign) {
+        if(s.size() <= i || !(isdigit(s[i]))) return (int) (sign * num);
+        const int INT_MAX_VAL = INT_MAX;
+        const int INT_MIN_VAL = INT_MIN;
+        num = num * 10 + (s[i] - '0');
+
+        if((sign * num) >= INT_MAX_VAL) return INT_MAX_VAL;
+        if((sign * num) <= INT_MIN_VAL) return INT_MIN_VAL;
+
+        return helper(s, i + 1, num, sign);
+    }
 public:
     int myAtoi(string s) {
-        if(s.empty()) return 0;
+        int i = 0;
 
-        const long long MAX_INT = INT_MAX;
-        const long long MIN_INT = INT_MIN;
+        while(i < s.size() && s[i] == ' ') i++;
 
-        int i = 0, n = s.length();
-
-        while(i < n && s[i] == ' ') i++;
-
-        if(i == n) return 0;
-
-        int sign = 1; 
-        if(s[i] == '+') i++;
-        else if(s[i] == '-') {
-            sign = -1;
+        int sign = 1;
+        if(i < s.size() && (s[i] == '-' || s[i] == '+')) {
+            sign = (s[i] == '-') ? -1 : 1;
             i++;
         }
 
-        long long res = 0;
-
-        while(i < n && isdigit(s[i])) {
-            int digit = s[i] - '0';
-            res = res * 10 + digit;
-
-            if(sign * res <= INT_MIN) return INT_MIN;
-            if(sign * res >= INT_MAX) return INT_MAX;
-
-            i++;
-        }
-
-        return static_cast<int>(res * sign);
+        return helper(s, i, 0, sign);
     }
 };
