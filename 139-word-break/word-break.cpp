6a93a10e479431756e13367c6763a286d653b5cb
AMOGHA M S <amogha@kvgce.ac.in>
2026-08-19 09:11:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (98.31%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
+        int maxLen = 0;
+
+        for(int i = 1; i <= s.size(); i++) {
+            for(const string& w: wordDict) {
+                int start = i - w.length();
+
+                if(start >= 0 && dp[start] && s.substr(start, w.length()) == w) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.size()];
+    }
+};
